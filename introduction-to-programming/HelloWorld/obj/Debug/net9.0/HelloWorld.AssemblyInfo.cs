5c33bda4633621260033f6bb9146f393daf85032
Juan Bernal <mesieou@gmail.com>
2025-03-06 14:46:22 +1100

    auto-generated files

--- introduction-to-programming/HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ introduction-to-programming/HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a6e2c3d8fbbfeaa9a00a4d21b23bed078e6c34f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+096a01a39ef474e6cfad3a7711457fa582563f24")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
