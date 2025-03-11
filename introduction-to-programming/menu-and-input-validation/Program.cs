8e82bc15810b1e435d848f077d555a6cdd17de3f
Juan Bernal <mesieou@gmail.com>
2025-03-12 07:50:04 +1100

    added logic to the menu and a while loop to stay in the menu until the user want to exit

---
+++ introduction-to-programming/menu-and-input-validation/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Convert;
+using static SplashKitSDK.SplashKit;
+
+string line;
+int choice;
+
+
+do
+{
+    WriteLine("1: Addition");
+    WriteLine("2: Substraction");
+    WriteLine("3: Multiplication");
+    WriteLine("4: Divition");
+    WriteLine("5: Quit");
+    WriteLine("Enter your Choice: ");
+    choice = ToInt32(ReadLine());
+    
+    switch(choice)
+    {
+        case 1: 
+        WriteLine("Addition");
+        break;
+        case 2:
+        WriteLine("Substraction");
+        break;
+        case 3:
+        WriteLine("Multiplication");
+        break;
+        case 4:
+        WriteLine("Divition");
+        break;
+        case 5:
+        WriteLine("Quit");
+        break;
+        default:
+        WriteLine("Incorrect input!. Please enter a number from the menu:");
+        break;
+    }
+} while(choice != 5);
