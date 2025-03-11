8aefa939554c8e9632174019a402cfa7eb0dbc2d
Juan Bernal <mesieou@gmail.com>
2025-03-11 17:35:17 +1100

    created the project and set up for the game

---
+++ introduction-to-programming/player-move-c#/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+using static SplashKitSDK.SplashKit;
+
+const int PLAYER_RADIUS = 50;
+
+OpenWindow("Player Moving", 1280,720);
+
+ClearScreen(ColorWhite());
+
+FillCircle(ColorTurquoise(), 640,360, PLAYER_RADIUS);
+
+RefreshScreen(60);
+
+Delay(5000);
+
+
+
+
