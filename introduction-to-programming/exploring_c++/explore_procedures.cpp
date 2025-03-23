e511b8829b61bee98f59a200d6cc746783c207aa
Juan Bernal <mesieou@gmail.com>
2025-03-24 09:41:09 +1100

    added a new function to print any text a number of times with a empty line at the end if needed

--- introduction-to-programming/exploring_c++/explore_procedures.cpp
+++ introduction-to-programming/exploring_c++/explore_procedures.cpp
@@ -1,18 +1,36 @@
 #include "splashkit.h"
 
-// Prints a line based on the user's input
+// prints anything a number of times with a space at the end if needed
+void print_repeated(string text, int times, bool with_newline)
+{
+  for (int i = 0; i < times; i++)
+  {
+    write(text);
+  }
+
+  if (with_newline)
+  {
+    write_line("\n");
+  }
+}
+
 void print_line(int length)
 {
-    for (int i = 0; i < length; i++)
-    {
-        write('-');
-    }  
+  print_repeated("-", length, true);
 }
 
 int main()
 {
   string input;
   int test_length;
+
+  print_line(20);
+  write_line("| Line print test  |");
+  print_line(20);
+
+  print_repeated("--+--+", 5, true);
+  print_repeated("Hello World\n", 5, false);
+  print_repeated("--+--+", 5, true);
 
   write("Enter a length for a test line: ");
   input = read_line();
