75d2d5e7b81ec510a6ca74e5400b2cc8ab42597b
Juan Bernal <mesieou@gmail.com>
2025-03-24 10:18:35 +1100

    added another function to read numbers from the user

--- introduction-to-programming/exploring_c++/explore_functions.cpp
+++ introduction-to-programming/exploring_c++/explore_functions.cpp
@@ -1,4 +1,5 @@
 #include "splashkit.h"
+using std::to_string;
 
 string read_string(string prompt)
 {
@@ -6,15 +7,32 @@ string read_string(string prompt)
     return read_line();
 }
 
+int read_integer(string prompt)
+{
+    string user_output = read_string(prompt);
+    while(!is_integer(user_output))
+    {
+        write_line("Please enter a whole number.");
+        user_output = read_string(prompt);
+    }
+    return convert_to_integer(user_output);
+}
+
 int main()
 {
   string name;
+  int age;
 
   // Read in a string and store it in name
   name = read_string("enter your name: ");
 
   // Output the name
   write_line("Hello " + name);
+
+  // Read in an integer
+  age = read_integer("what is your age: ");
+
+  write_line("You entered " + to_string(age));
 
   return 0;
 }
