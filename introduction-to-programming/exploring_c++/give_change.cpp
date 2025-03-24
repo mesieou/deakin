febed40a442f0067c148123fc7337f6237af75b0
Juan Bernal <mesieou@gmail.com>
2025-03-24 16:43:59 +1100

    Added the give_change procedure receiving a change integer as parameter and displaying to the user the change

---
+++ introduction-to-programming/exploring_c++/give_change.cpp
@@ -0,0 +1,55 @@
+#include "splashkit.h"
+#include <sstream>  // For stringstream
+
+using std::to_string;
+using namespace std;
+
+const int NUM_COIN_TYPES = 6;
+
+const int TWO_DOLLARS = 200;
+const int ONE_DOLLAR = 100;
+const int FIFTY_CENTS = 50;
+const int TWENTY_CENTS = 20;
+const int TEN_CENTS = 10;
+const int FIVE_CENTS = 5;
+
+void give_change(int change)
+{
+    int coin_value;
+    int curr_change = change;
+    string coin_text;
+    
+    for(int i = 0; i < NUM_COIN_TYPES; i++)
+    {
+        switch (i)
+        {
+        case 0:
+            coin_value = TWO_DOLLARS;
+            coin_text = "$2, ";
+            break;
+        case 1:
+            coin_value = ONE_DOLLAR;
+            coin_text = "$1, ";
+            break;
+        case 2:
+            coin_value = FIFTY_CENTS;
+            coin_text = "50c, ";
+            break;
+        case 3:
+            coin_value = TWENTY_CENTS;
+            coin_text = "20c, ";
+            break;
+        case 4:
+            coin_value = TEN_CENTS;
+            coin_text = "10c, ";
+            break;
+        case 5:
+            coin_value = FIVE_CENTS;
+            coin_text = "5c, ";
+            break;
+        }
+        int coin_quantity = floor(curr_change / coin_value);
+        curr_change -= coin_value * coin_quantity;
+        write_line(to_string(curr_change) + " x " + coin_text);
+    }
+}
