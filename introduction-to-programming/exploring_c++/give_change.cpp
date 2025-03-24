f231aed291af0a1a6ed8c8cbc82b8ac50b8906c2
Juan Bernal <mesieou@gmail.com>
2025-03-24 18:07:42 +1100

    moved the read_integer and read_string methods to utilities

--- introduction-to-programming/exploring_c++/give_change.cpp
+++ introduction-to-programming/exploring_c++/give_change.cpp
@@ -1,11 +1,11 @@
+#include "utilities.h"
 #include "splashkit.h"
-#include <sstream>  // For stringstream
+#include <string>
 
+using std::string;
 using std::to_string;
-using namespace std;
 
 const int NUM_COIN_TYPES = 6;
-
 const int TWO_DOLLARS = 200;
 const int ONE_DOLLAR = 100;
 const int FIFTY_CENTS = 50;
@@ -52,37 +52,6 @@ void give_change(int change)
         curr_change -= coin_value * coin_quantity;
         write_line(to_string(coin_quantity) + " x " + coin_text);
     }
-}
-
-string read_string(string prompt)
-{
-    write_line(prompt);
-    return read_line();
-}
-
-int read_integer(string prompt, 
-                 int from = numeric_limits<int>::min(), 
-                 int to = numeric_limits<int>::max())
-{
-    string user_output = read_string(prompt);
-    
-    while(!is_integer(user_output))
-    {
-        write_line("Please enter a whole number./n");
-        user_output = read_string(prompt);
-    }
-
-    int user_output_int = convert_to_integer(user_output);
-    while (!(user_output_int > from and user_output_int < to))
-    {
-        stringstream ss;
-        ss << "Please enter a number from " << from << " to " << to;
-
-        write_line( ss.str());
-        user_output_int = convert_to_integer(read_string(prompt));
-    } 
-
-    return user_output_int;
 }
 
 
--- introduction-to-programming/exploring_c++/perform_guess_game.cpp
+++ introduction-to-programming/exploring_c++/perform_guess_game.cpp
@@ -1,42 +1,9 @@
 #include "splashkit.h"
-#include <sstream>  // For stringstream
-
-using std::to_string;
-using namespace std;
+#include "utilities.h"
 
 const int MAX_NUMBER = 100;
 const int MAX_GUESSES = 7;
 
-string read_string(string prompt)
-{
-    write_line(prompt);
-    return read_line();
-}
-
-int read_integer(string prompt, 
-                 int from = numeric_limits<int>::min(), 
-                 int to = numeric_limits<int>::max())
-{
-    string user_output = read_string(prompt);
-    
-    while(!is_integer(user_output))
-    {
-        write_line("Please enter a whole number");
-        user_output = read_string(prompt);
-    }
-
-    int user_output_int = convert_to_integer(user_output);
-    while (!(user_output_int > from and user_output_int < to))
-    {
-        stringstream ss;
-        ss << "Please enter a number from " << from << " to " << to;
-
-        write_line( ss.str());
-        user_output_int = convert_to_integer(read_string(prompt));
-    } 
-
-    return user_output_int;
-}
 
 bool perform_guess(int attempt_number, int target)
 {
