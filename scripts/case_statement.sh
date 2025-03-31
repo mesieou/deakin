ee840358072afb85016591064b682cc173027e00
Juan Bernal <mesieou@gmail.com>
2025-04-01 08:53:16 +1100

    Update case_statement.sh

--- scripts/case_statement.sh
+++ scripts/case_statement.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-echo "Choose an option: Start, Stop, Status"
+
+echo "Choose an option: Start, Stop, Status, Quit"
 read option
 
-case $option in
-  "Start")
-    echo "System starting";;
-  "Stop")
-    echo "System stopping...";;
-  "Status")
-    echo "System status: All systems go!";;
-esac
+while [ $option != "Quit" ]; do
+    case $option in
+    "Start")
+        echo "System starting";;
+    "Stop")
+        echo "System stopping...";;
+    "Status")
+        echo "System status: All systems go!";;
+    esac
+    echo "Choose an option: Start, Stop, Status, Quit"
+    read option
+done
+
