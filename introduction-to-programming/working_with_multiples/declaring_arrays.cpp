59412b3137b828ea986e11952840db82c1bf79da
Juan Bernal <mesieou@gmail.com>
2025-04-09 09:04:46 +1000

    created the shortest_name function

--- introduction-to-programming/working_with_multiples/declaring_arrays.cpp
+++ introduction-to-programming/working_with_multiples/declaring_arrays.cpp
@@ -12,24 +12,29 @@ int total_length( string arr[], int size)
     return result;
 }
 
+string shortest_name(string arr[], int size) {
+    string min = arr[0];
+
+    for (int i = 1; i < size; i++)
+    {
+        if (length_of(min) > length_of(arr[i]))
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+ }
+
+
+
 int main()
 {
     const int SIZE = 3;
-    string my_first_array[3] = {"Juan", "David", "Bernal"};
+    string my_first_array[3] = {"Ju", "David", "Bernal"};
      
-    int index = 0;
-    while (index < SIZE )
-    {
-        write_line(my_first_array[index]);
-        index++;
-        
-    }
-    
-    for (int i = 0; i < SIZE; i++)
-    {
-        write_line(my_first_array[i]);
-    }
 
     int total_length_names = total_length(my_first_array, SIZE);
-    write_line(total_length_names);
+    string name = shortest_name(my_first_array, SIZE);
+    write_line(name);
+
 }
