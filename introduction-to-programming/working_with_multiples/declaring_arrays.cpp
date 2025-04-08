b1bcebdc315223b6bf879d26e0d5f11f5af79123
Juan Bernal <mesieou@gmail.com>
2025-04-08 16:39:41 +1000

    created a project for working with tuples

---
+++ introduction-to-programming/working_with_multiples/declaring_arrays.cpp
@@ -0,0 +1,9 @@
+#include "utilities.h"
+#include "splashkit.h"
+
+int main()
+{
+    string my_first_array[3] = {"Juan", "David", "Bernal"};
+
+    write_line(my_first_array[0]);
+}
---
+++ introduction-to-programming/working_with_multiples/utilities.h
@@ -0,0 +1,16 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <string>
+#include <sstream>  // For stringstream
+
+using std::to_string;
+using namespace std;
+using std::string;
+
+string read_string(string prompt);
+
+int read_integer(string prompt, 
+                 int from = numeric_limits<int>::min(), 
+                 int to = numeric_limits<int>::max());
+#endif
