3a21a6bf680c3e86ea35931eb948caea52e5ec83
Juan Bernal <mesieou@gmail.com>
2025-04-08 11:26:35 +1000

    Created a couple of methods to read the user’s role and display it to the user

--- introduction-to-programming/indirect_access/exploring_references.cpp
+++ introduction-to-programming/indirect_access/exploring_references.cpp
@@ -2,7 +2,6 @@
 #include <sstream>
 #include <cstdlib> 
 
-using std::to_string;
 using namespace std;
 
 //permission levels structure
@@ -45,7 +44,7 @@ int read_integer(string prompt,
     }
 
     int user_output_int = convert_to_integer(user_output);
-    while (!(user_output_int > from and user_output_int < to))
+    while (!(user_output_int > from and user_output_int <= to))
     {
         stringstream ss;
         ss << "Please enter a number from " << from << " to " << to;
@@ -56,12 +55,66 @@ int read_integer(string prompt,
     return user_output_int;
 }
 
+string permission_to_string( permissions_level_data permission)
+{
+    switch (permission)
+    {
+    case NO_ACCESS:
+        return "No access";
+        break;
+    case USER:
+        return "USER";
+        break;
+    case ADMIN:
+        return "Admin";
+        break;
+    case SUPER_USER:
+        return "Super User";
+        break;
+    
+    default:
+        return "Unknown role";
+    }
+}
+
+permissions_level_data read_role()
+{
+    int role_input = read_integer("Enter rol level(0 for none, 1 for USER, 2 for Admin)", 0, 2);
+
+    switch (role_input)
+    {
+    case 0:
+        return NO_ACCESS;
+        break;
+    case 1:
+        return USER;
+        break;
+    case 2:
+        return ADMIN;
+        break;
+    default:
+        return NO_ACCESS;
+    }
+}
+
+void print_user( user_data user)
+{
+    write_line("");
+    write_line("User name: " + user.username);
+    write_line("Role: " + permission_to_string(user.permissions_level));
+}
+
+
+
 int main()
 {
-  user_data user = {
-    "my_user_name",
-    USER
-  };
+    
+    permissions_level_data role = read_role();
+    user_data user = {
+        "my_user_name",
+        "sfafas", // username
+        role            // permission level
+    };
 
   print_user(user);
 
