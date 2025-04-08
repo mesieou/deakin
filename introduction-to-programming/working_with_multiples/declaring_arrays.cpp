b9674c069d4b92f52f230820fefa85ada176d54e
Juan Bernal <mesieou@gmail.com>
2025-04-08 17:09:24 +1000

    Used a for loop to access all the elements in the array

--- introduction-to-programming/working_with_multiples/declaring_arrays.cpp
+++ introduction-to-programming/working_with_multiples/declaring_arrays.cpp
@@ -1,9 +1,24 @@
 #include "utilities.h"
 #include "splashkit.h"
 
+const int SIZE = 3;
+
 int main()
 {
     string my_first_array[3] = {"Juan", "David", "Bernal"};
-
-    write_line(my_first_array[0]);
+     
+    int index = 0;
+    while (index < SIZE )
+    {
+        write_line(my_first_array[index]);
+        index++;
+        
+    }
+    
+    for (int i = 0; i < SIZE; i++)
+    {
+        write_line(my_first_array[i]);
+    }
+    
+    
 }
