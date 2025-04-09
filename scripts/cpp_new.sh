30a2f6b4cbe98024bd62e568502ef4fb97f3f2d1
Juan Bernal <mesieou@gmail.com>
2025-04-09 10:49:28 +1000

    updated the boilerplate for cpp script

--- scripts/cpp_new.sh
+++ scripts/cpp_new.sh
@@ -8,10 +8,26 @@ mkdir $folder && cd $folder
 
 echo "📂 Creating main file and bringing utilities..."
 touch main.cpp
-cp ~/code/mesieou/2025/deakin/introduction-to-programming/utilities/utilities.cpp $folder
-cp ~/code/mesieou/2025/deakin/introduction-to-programming/utilities/utilities.h $folder
+cp ~/code/mesieou/2025/deakin/introduction-to-programming/utilities/utilities.cpp ./
+cp ~/code/mesieou/2025/deakin/introduction-to-programming/utilities/utilities.h ./
 
 echo '#include "utilities.h"' >> main.cpp
 echo '#include "splashkit.h"' >> main.cpp
 
+# Add the main function to main.cpp
+echo '' >> main.cpp
+echo 'int main() {' >> main.cpp
+echo '    // Your code goes here' >> main.cpp
+echo '    return 0;' >> main.cpp
+echo '}' >> main.cpp
+
+# pushing the set up to github
+echo "Pushing to Github the  new project..."
+
+git add .
+git commit -m "created and set up a new project for $folder"
+branch=$(git rev-parse --abbrev-ref HEAD)
+git push origin "$branch"
+
 code .
+
