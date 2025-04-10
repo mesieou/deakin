ce20133f3b5fc44f26a99dad84138aa6fba55ee6
Juan Bernal <mesieou@gmail.com>
2025-04-10 17:42:49 +1000

    added menu and initialise store methods

--- introduction-to-programming/product_database/main.cpp
+++ introduction-to-programming/product_database/main.cpp
@@ -16,7 +16,7 @@ typedef struct
 typedef struct
 {
     product products[MAX_PRODUCTS];
-    int products_number;
+    int type_products_quantity;
     int sales;
     double profit;
 } store_data;
@@ -31,7 +31,7 @@ int search(store_data &store, string term) {
     strings_list matched_indexes = {{},0};
 
     // look through all the products array
-    for (int i = 0; i < store.products_number; i++)
+    for (int i = 0; i < store.type_products_quantity; i++)
     {
         
         // check if any products contains the search term
@@ -64,46 +64,121 @@ void add_product(store_data &store) {
     int product_index = search(store, name);
     
     // check if the product already exists
-    if (product_index == -1)
+    if (product_index != -1)
     {
         double cost = read_double("Enter the cost:");
         double price = read_double("Enter the sale price:");
-        double profit = price - cost;
+        int quantity = read_integer("Quantity:");
+        
         //creates the new product
-        product new_product = {name, cost, price,1};
+        product new_product = {name, cost, price,quantity};
         //adds the product to the store
-        store.products[store.products_number] = new_product;
-        //adds the profit to the store
+        store.products[store.type_products_quantity] = new_product;
         write_line("Product added!");
     } else
     {
         write_line("The following product already exists:");
         write_line("- " + store.products[product_index].name);
-        string ans = read_string("Do you want to add one? [yes / no]: ");
+        string ans = read_string("Do you want to add more to the stock? [yes / no]: ");
+        
         // checks if the customer wants to increase the quantity of the existed product
         if ( to_lowercase(ans) == "yes")
         {
-            double profit = price - cost;
-             
-            store.products[product_index].quantity++;
+            int quantity = read_integer("Quantity:");
+            
+            //adds more products of the same category
+            store.products[product_index].quantity += quantity;
+            
             write_line("Done!");
         }
         write_line("Bye bye then!");
     }
 }
 
-int main() {
-    // Your code goes here
-    product house_move = {"House Move", 70, 140, 1};
-    product business_move = {"business Move", 70, 140, 1};
-    store_data store = {{house_move}, 1, 140,70};
-
-    store.products[0] = house_move;
-    store.products[1] = business_move;
-    store.products_number += 2;
+void print_menu()
+{
+    printf("\nMenu\n");
+    printf("1. Add a new product\n");
+    printf("2. Delete a product\n");
+    printf("3. Update a product\n");
+    printf("4. Sell a product\n");
+    printf("5. Quit\n");
     
-    int chosen_index = search(store);
-    write_line(store.products[chosen_index].name);
+}
+
+//todo: add populate_array here
+store_data initialise_the_store() {
+    write_line("Creating your store...");
+    store_data store = {{},0, 0,0};
+    
+    int size = read_integer("How many different products you would like to enter?");
+    
+    //checks the user do not initialise the store with more than the quantity of products allowed(100)
+    while (size > MAX_PRODUCTS)
+    {
+        write_line("Sorry the max number is 20");
+        size = read_integer("How many different products you would like to enter?");
+    }
+    
+    while (size < 0)
+    {
+        write_line("Only positive numbers allowed.");
+        size = read_integer("How many different products you would like to enter?");
+    } 
+    
+    //setting the products quantity of the store
+    store.type_products_quantity = size;
+    
+    // creating all the products
+    for (int i = 0; i < store.type_products_quantity; i++)
+    {
+        printf("Product %d:\n", i + 1);
+        string name = read_string("Name:");
+        double cost = read_double("Cost:");
+        double price = read_double("Sale Price:");
+        int quantity = read_integer("Quantity:");
+
+        //create the product
+        product item = {name, cost, price, quantity};
+
+        //save the product in the store
+        store.products[i] = item;
+    }
+    return store;
+}
+
+int main() {
+    // asks customer the initial state of the store
+    store_data store = initialise_the_store();
+    
+    int option;
+    do
+    {
+        
+        print_menu();
+        option = read_integer("Enter a number:");
+        switch (option)
+        {
+        case 1:
+            add_product(store);
+            break;
+        // case 2:
+        //     remove_at_index(data);
+        //     break;
+        // case 3:
+        //     print(data);
+        //     break;
+        // case 4:
+        //     calculate_stats(data);
+        //     break;
+        case 5:
+            printf("bye bye");
+            break;
+        default:
+            write_line("Unknown ooption, please enter a number from 1 to 5.");
+            break;
+        }
+    } while (option != 5);
 
     return 0;
 }
