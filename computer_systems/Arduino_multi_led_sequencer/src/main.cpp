56ad1c3ccc352ee9a071b356b2b797da408524fe
Juan Bernal <mesieou@gmail.com>
2025-04-12 14:18:21 +1000

    finished the multiled sequencer

--- computer_systems/Arduino_multi_led_sequencer/src/main.cpp
+++ computer_systems/Arduino_multi_led_sequencer/src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Define LED pins
+int led1 = 2;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(led1, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  digitalWrite(led1, LOW);
+  delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
