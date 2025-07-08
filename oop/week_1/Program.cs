95ef850990a1807c8947ddd1328d3276a840d08a
Juan Bernal <mesieou@gmail.com>
2025-07-08 18:38:22 +1000

    initialised first project

---
+++ oop/week_1/Program.cs
@@ -0,0 +1,8 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for (int i = 10 - 1; i >= 0; i--)
+{
+    Console.WriteLine($"Current number: {i}");
+}
