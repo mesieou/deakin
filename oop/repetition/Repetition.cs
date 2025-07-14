7d6e7d1f91ad351ca6bb9f2e1789c0d0fe696b77
Juan Bernal <mesieou@gmail.com>
2025-07-14 12:33:18 +1000

    practicing while and for loops with c#

---
+++ oop/repetition/Repetition.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using System.Threading.Tasks.Dataflow;
+
+class Repetition
+{
+    static void Main(string[] args)
+    {
+        int sumForLoop = 0;
+        double avergageForLoop;
+        int upperbound = 100;
+
+        for (int num = 1; num <= upperbound; num++)
+        {
+            sumForLoop += num;
+        }
+        avergageForLoop = (double)sumForLoop / upperbound;
+
+        int number = 1;
+        int sumWhileLoop = 0;
+        double avergageWhileLoop;
+
+
+        while (number <= upperbound)
+        {
+            sumWhileLoop += number;
+            number++;
+        }
+        avergageWhileLoop = (double)sumWhileLoop / upperbound;
+
+        int n = 1;
+        int sumDoWhileLoop = 0;
+        double avergageDoWhileLoop;
+
+
+        do
+        {
+            sumDoWhileLoop += n;
+            n++;
+        } while (n <= upperbound);
+
+        avergageDoWhileLoop = (double)sumDoWhileLoop / upperbound;
+
+
+        Console.WriteLine(sumForLoop);
+        Console.WriteLine(avergageForLoop);
+
+        Console.WriteLine(sumForLoop);
+        Console.WriteLine(avergageWhileLoop);
+
+        Console.WriteLine(sumDoWhileLoop);
+        Console.WriteLine(avergageDoWhileLoop);
+    }
+}
