5abc68cb74ec194a1257f62bd9bf569bcded1cdf
Juan Bernal <mesieou@gmail.com>
2025-07-21 10:29:39 +1000

    improved the guessing nummber exercise

--- oop/guessing-numbers/GuessingNumber.cs
+++ oop/guessing-numbers/GuessingNumber.cs
@@ -1,21 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 class GuessingNumber
 {
-    static void Main(string[] args)
+    //helper function to get a valid integer
+    static int GetInt(string prompt)
     {
-        int number = 5;
         int input = 0;
+        bool inputIsValid = false;
+
         do
         {
             try
             {
-                Console.WriteLine("Guess a number between 1 and 10: ");
+                Console.WriteLine(prompt);
                 input = Convert.ToInt32(Console.ReadLine());
-                if (input == number)
-                {
-                    Console.WriteLine("Congrats!, you have guessed the number!.");
-                }
-                else if (input > 10)
+                if (input > 10)
                 {
                     Console.WriteLine("You have entered a number greater than 10.");
                 }
@@ -23,14 +21,43 @@ class GuessingNumber
                 {
                     Console.WriteLine("You have entered a number less than 1");
                 }
-
+                else
+                {
+                    inputIsValid = true;
+                }
             }
             catch (FormatException)
             {
                 Console.WriteLine("That is not a valid integer.");
+                inputIsValid = false;
             }
 
+        } while (!inputIsValid);
 
-        } while (input != number);
+        return input;
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        //Takes the user 1 input and checks is valid.
+        int inputUser1 = GetInt("User 1, type a number between 1 and 10 for user 2 to guess: ");
+
+        //Takes the user 2 input and checks is valid.
+        int inputUser2 = GetInt("User 2, type a number between 1 and 10: ");
+
+        //Checks user 1 input against user 2 input until guessed.
+        do
+        {
+            if (inputUser1 == inputUser2)
+            {
+                Console.WriteLine("Congrats!, you have guessed the number!.");
+            }
+            else
+            {
+                Console.WriteLine("Keep trying");
+            }
+        } while (inputUser1 != inputUser2);
     }
 }
