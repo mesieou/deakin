1dd7499e7037a4c092bc24ad13718f95387111b6
Juan Bernal <mesieou@gmail.com>
2025-07-31 10:03:29 +1000

    updated the withdraw method of the banking system to check if the balance is not enough

---
+++ oop/banking-system/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Account
+{
+    private string name;
+    private double balance;
+
+    public Account(string name, double balance)
+    {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void Deposit(double amount)
+    {
+        this.balance += amount;
+    }
+
+    public void Withdraw(double amount)
+    {
+
+        if (this.balance - amount > 0)
+        {
+            this.balance -= amount;
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, not enough balance to withdraw {amount}");
+            Console.WriteLine($"Your current balance is {this.balance}");
+            Console.WriteLine();
+        }
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Account Name: {this.name}");
+        Console.WriteLine($"Account Balance: {this.balance}");
+        Console.WriteLine();
+    }
+
+    public string Name()
+    {
+        return this.name;
+    }
+}
+
+class TestAccount()
+{
+    static void Main()
+    {
+        //test account initialisation
+        Account savings = new Account("Savings", 350);
+        savings.Print();
+
+        //test deposit
+        savings.Deposit(54);
+        savings.Print();
+
+        //test withdrawal
+        savings.Withdraw(30);
+        savings.Print();
+
+        //test to withdraw more than the current balance
+        savings.Withdraw(380);
+        savings.Print();
+
+        //test getting the name
+        string savingsName = savings.Name();
+        Console.WriteLine(savingsName);
+    }
+}
---
+++ oop/exceptions/Program.cs
@@ -0,0 +1,109 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+class Account
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public int Balance { get; private set; }
+
+    public Account(string firstName, string lastName, int balance)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Balance = balance;
+    }
+
+    public void Withdraw(int amount)
+    {
+        if (amount > Balance)
+        {
+            //InvalidOperationException
+            throw new InvalidOperationException("Insufficient fund");
+        }
+        Balance -= amount;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //NullReferenceException
+        Account myAccount = null;
+
+        try
+        {
+            myAccount.Withdraw(50);
+        }
+        catch (NullReferenceException ex)
+        {
+            Console.WriteLine("NullReferenceException found: " + ex.Message);
+        }
+
+        //index out of reference
+        int[] array = new int[100];
+        try
+        {
+            array[101] = 33;
+
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+
+            Console.WriteLine("IndexOutOfRangeException found: " + ex.Message);
+        }
+
+        //OutOfMemoryException
+        string value = new string('a', int.MaxValue);
+    }
+
+
+}
+
+
+
+class StackOverflowException
+{
+
+}
+
+class OutOfMemoryException
+{
+
+}
+
+class DivideByZeroException
+{
+
+}
+
+class ArgumentNullException
+{
+
+}
+
+class ArgumentOutOfRangeException
+{
+
+}
+
+class FormatException
+{
+
+}
+
+class ArgumentException
+{
+
+}
+
+class SystemException
+{
+
+}
+
+
+
+
+
