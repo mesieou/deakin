0a2039dfe61e272dec69470e00bed7a83b2fcace
Juan Bernal <mesieou@gmail.com>
2025-08-04 11:12:30 +1000

    improved the withdraw and deposit functions of the bankin systems so that it cannot deposit negative numbers and it cannot withdraw more money than the balance

---
+++ oop/banking-system2/Program.cs
@@ -0,0 +1,44 @@
+﻿class Account
+{
+    private string name;
+    private double balance;
+
+    public Account(string name, double balance)
+    {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public bool Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            this.balance += amount;
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    public bool Withdraw(double amount)
+    {
+
+        if (this.balance - amount > 0)
+        {
+            this.balance -= amount;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, not enough balance to withdraw {amount}");
+            Console.WriteLine($"Your current balance is {this.balance}");
+            Console.WriteLine();
+            return false;
+        }
+    }
+
+    class BankSystem()
+    {
+
+    }
