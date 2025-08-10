9ded3157cbe381f347145e6367ea528b532b2344
Juan Bernal <mesieou@gmail.com>
2025-08-10 10:32:54 +1000

    updated banking system 1 and 2

--- oop/banking-system/obj/Debug/net9.0/banking-system.AssemblyInfo.cs
+++ oop/banking-system/obj/Debug/net9.0/banking-system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("banking-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4be0edc50cb26e944f2670602ad58af2285e03cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeb65aaa5514be48c497bd00780c9cd05b8cb23d")]
 [assembly: System.Reflection.AssemblyProductAttribute("banking-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("banking-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
